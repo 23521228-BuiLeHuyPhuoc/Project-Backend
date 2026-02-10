d597ad3db6afc0b7e2ec32785c36478d61f85287
Bùi Bùi <huyphuoc09112005@gmail.com>
2026-02-10 22:33:48 +0700

    Tich hop

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const express = require('express')
+
+const path=require('path');
+
 const app = express()
 const port = 3000
 
+
+app.set('views', path.join(__dirname, "views"));
+
+app.set('view engine','pug');
+
 app.get('/', (req, res) => {
-  res.send('Trang chủ')
+  res.render('client/pages/home',{
+    pageTitle:"Trang chủ"
+  })
 })
 
+
 app.get('/tours', (req, res) => {
-  res.send('Danh sách tour')
+  res.render('client/pages/tour-list',{
+    pageTitle:"Danh sách tour"
+  });
 })
 
 app.listen(port, () => {
