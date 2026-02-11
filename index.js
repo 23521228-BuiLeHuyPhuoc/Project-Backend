566e91fcec391e05b3735e499b728c04dd277b94
Bùi Bùi <huyphuoc09112005@gmail.com>
2026-02-11 20:36:42 +0700

    Kho hieu

--- index.js
+++ index.js
@@ -4,18 +4,19 @@ const path=require('path');
 const mongoose=require('mongoose');
 
 mongoose.connect(process.env.DATABASE)
+
+const clientRoutes=require('./routes/client/index.route');
+
 const app = express()
 
 const port = 3000
-const tourController=require('./controllers/client/tour.controller');
-const homeController=require('./controllers/client/home.controller');
+
 app.set('views', path.join(__dirname, "views"));
 
 app.set('view engine','pug');
 
+app.use('/',clientRoutes);
 
-app.get('/', homeController.home );
-app.get('/tours', tourController.list );
 
 app.use(express.static(path.join(__dirname,'public')));
 
