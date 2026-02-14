278dcf140a7859015858fb6bd29e2a8e2fe017a8
Bùi Bùi <huyphuoc09112005@gmail.com>
2026-02-14 10:36:22 +0700

    Tich hop gio hang

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -4,6 +4,9 @@ const tourRoutes=require('./tour.route');
 
 const homeRoutes=require('./home.route');
 
+const cartRoutes=require('./cart.route');
+
+router.use('/cart', cartRoutes );
 
 router.use('/tours', tourRoutes );
 router.use('/', homeRoutes );
