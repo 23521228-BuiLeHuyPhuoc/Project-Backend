e245058a2f1b540e0a6a6b70767df169edca7184
Bùi Bùi <huyphuoc09112005@gmail.com>
2026-02-15 06:45:22 +0700

    Tich hop tinh nang Dang ky

--- controllers/admin/account.controller.js
+++ controllers/admin/account.controller.js
@@ -1,5 +1,10 @@
-module.exports.account=(req,res)=>{
+module.exports.login=(req,res)=>{
     res.render("admin/pages/login.pug",{
         pageTitle:"Đăng nhập"
+    })
+}
+module.exports.register=(req,res)=>{
+    res.render("admin/pages/register.pug",{
+        pageTitle:"Đăng ký"
     })
 }
--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -3,7 +3,6 @@ const router=require('express').Router();
 const accountRoutes=require('./account.route');
 
 
-router.use('/account/login', accountRoutes);
 router.use('/account', accountRoutes);
 
 module.exports=router;
