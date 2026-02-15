f2dcc34b079ff7d4c84d0ff1f2be272a83dbc98d
Bùi Bùi <huyphuoc09112005@gmail.com>
2026-02-15 14:31:19 +0700

    Tich hop Order list va order create

---
+++ controllers/admin/tour.controller.js
@@ -0,0 +1,15 @@
+module.exports.list=(req,res)=>{
+    res.render("admin/pages/tour-list",{
+        pageTitle:"Quản lý tour"
+    })
+}
+module.exports.create=(req,res)=>{
+    res.render("admin/pages/tour-create",{
+        pageTitle:"Tạo mới tour"
+    })
+}
+module.exports.trash=(req,res)=>{
+    res.render("admin/pages/tour-trash",{
+        pageTitle:"Thùng rác tour"
+    })
+}
