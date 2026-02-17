0376fe2533fc4595ff940e8fc2b6a24f2486a0d9
Bùi Bùi <huyphuoc09112005@gmail.com>
2026-02-17 22:19:38 +0700

    Tich hop validate

--- validates/admin/account.validate.js
+++ validates/admin/account.validate.js
@@ -1,25 +1,57 @@
 const joi=require('joi');
 module.exports.registerPost=async(req,res,next)=>{
     const schema=joi.object({
-        fullName:joi.string().required().messages({
+        fullName:joi.string().required()
+        .min(5)
+        .max(50)        
+        .messages({
         "string.empty":"Vui lòng nhập họ tên!",
-
+        "string.min":"Họ tên phải có ít nhất 5 ký tự!",
+        "string.max":"Họ tên không được vượt quá 50 ký tự!"
         }),
-        email:joi.string().required().messages({
+        email:joi.string().required().email().messages({
             "string.empty":"Vui lòng nhập email!",
-
+            "string.email":"Vui lòng nhập đúng định dạng email!"
         }),
-        password:joi.string().required().messages({
-            "string.empty":"Vui lòng nhập mật khẩu!",
+        password:joi.string().required()
+        .min(8)
+        .custom((value,helpers)=>{
+        if(!/[A-Z]/.test(value))
+        {
+            return helpers.error("password.uppercase");
+        }
+        if(!/[a-z]/.test(value)){
+            return helpers.error("password.lowercase");
+        }
+        
+        if(!/[0-9]/.test(value)){
+            return helpers.error("password.digit");
+        }
+        if(!/[!@#$%^&*()<>?,./_+=-]/.test(value)){
+            return helpers.error("password.special");
+        }
+        return value;
+    })
+        .messages({
+        "string.empty":"Vui lòng nhập mật khẩu!",
+        "string.min":"Mật khẩu phải có ít nhất 8 ký tự!",
+        "password.uppercase":"Mật khẩu phải chứa ít nhất một chữ cái viết hoa!",
+        "password.lowercase":"Mật khẩu phải chứa ít nhất một chữ cái viết thường!",
+        "password.digit":"Mật khẩu phải chứa ít nhất một chữ số!",
+        "password.special":"Mật khẩu phải chứa ít nhất một ký tự đặc biệt!"        
         }),
 
     })
     const {error}=schema.validate(req.body,{abortEarly:false});
+    console.log(error);
     if(error){
-        console.log(error);
+         const errorMessage=error.details.map(err=>{
+            return err.message;
+         }).join("\n");
+        
         res.json({
             code:"error",
-            message:"Lỗi!"
+            message: errorMessage
         });
         return;
     }
