8ce393df8df915f4d3b9a2f9208884a7d7c71078
Bùi Bùi <huyphuoc09112005@gmail.com>
2026-02-25 09:48:39 +0700

    Them phan cap danh muc

--- routes/admin/category.route.js
+++ routes/admin/category.route.js
@@ -1,6 +1,6 @@
 const router=require('express').Router();
 const multer=require('multer');
-
+const categoryValidate=require('../../validates/admin/category.validate');
 const cloudinaryHelper=require('../../helpers/cloudinary.helper');
 const upload=multer({storage:cloudinaryHelper.storage});
 
@@ -8,5 +8,5 @@ const categoryController=require('../../controllers/admin/category.controller');
 
 router.get('/list',categoryController.list);
 router.get('/create',categoryController.create);
-router.post('/create',upload.single('avatar'),categoryController.createPost);
+router.post('/create',upload.single('avatar'),categoryValidate.createPost,categoryController.createPost);
 module.exports=router;
