b3172fe42787f68dcd35c3e0028017ad79f45482
Bùi Bùi <huyphuoc09112005@gmail.com>
2026-02-26 18:52:07 +0700

    Loc theo ngay tao

--- controllers/admin/category.controller.js
+++ controllers/admin/category.controller.js
@@ -14,10 +14,28 @@ module.exports.list=async (req,res)=>{
     {
         find.createdBy=req.query.creator;
     }
+    const dateFilter={};
+    if(req.query.startDate)
+    {
+    const startDate=moment(req.query.startDate).startOf("date").toDate();
+    dateFilter.$gte=startDate;
+    }
+    if(req.query.endDate)
+    {
+        const endDate=moment(req.query.endDate).endOf("date").toDate();
+        dateFilter.$lte=endDate;
+
+    }
+    if(Object.keys(dateFilter).length>0)
+    {
+        find.createdAt=dateFilter;
+    }
     const categoryList=await Category.find(find).sort({
         position: "asc"
     })
-    const creator=await AccountAdmin.find({});
+    const creator=await AccountAdmin.find({
+
+    }).select("id fullName");
     for (const item of categoryList) {
         if(item.createdBy){
             const accountCreated=await AccountAdmin.findOne({
--- public/admin/assets/js/script.js
+++ public/admin/assets/js/script.js
@@ -808,3 +808,48 @@ if(filterCreator){
 
 
 //End Filter Creator
+//Filter StartDate
+const filterStartDate=document.querySelector("[filter-start-date]");
+if(filterStartDate){
+  const url=new URL(window.location.href);
+
+  filterStartDate.addEventListener("change",()=>{
+    const value=filterStartDate.value;
+    if(value){
+      url.searchParams.set("startDate",value);
+    }else{
+      url.searchParams.delete("startDate");
+    }
+    window.location.href=url.href;
+
+  })
+  // Hiển thị lựa chọn mặc định 
+  const valueCurrent=url.searchParams.get("startDate");
+  if(valueCurrent){
+    filterStartDate.value=valueCurrent;
+  }
+}
+//End Filter StartDate
+
+//Filter EndDate
+const filterEndDate=document.querySelector("[filter-end-date]");
+if(filterEndDate){
+  const url=new URL(window.location.href);
+
+  filterEndDate.addEventListener("change",()=>{
+    const value=filterEndDate.value;
+    if(value){
+      url.searchParams.set("endDate",value);
+    }else{
+      url.searchParams.delete("endDate");
+    }
+    window.location.href=url.href;
+
+  })
+  // Hiển thị lựa chọn mặc định 
+  const valueCurrent=url.searchParams.get("endDate");
+  if(valueCurrent){
+    filterEndDate.value=valueCurrent;
+  }
+}
+//End Filter EndDate
