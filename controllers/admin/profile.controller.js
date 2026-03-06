62356cf1c37e675bc3a589e0d3db03f9ade62d2b
Bùi Bùi <huyphuoc09112005@gmail.com>
2026-03-06 21:26:46 +0700

    Hoan thanh trang thong tin ca nhan

--- controllers/admin/profile.controller.js
+++ controllers/admin/profile.controller.js
@@ -1,10 +1,45 @@
-module.exports.edit=(req,res)=>{
+const AccountAdmin = require("../../models/account-admin.model");
+const Role=require("../../models/roles.model");
+module.exports.edit=async (req,res)=>{
+    const roleList=await Role.find({
+        deleted:false,
+    })
     res.render("admin/pages/profile-edit",{
-        pageTitle:"Thông tin cá nhân"
+        pageTitle:"Thông tin cá nhân",
+        roleList:roleList
     })
 }
 module.exports.changePassword=(req,res)=>{
     res.render("admin/pages/profile-change-password",{
         pageTitle:"Đổi mật khẩu"
     })
+}
+module.exports.editPatch=async(req,res)=>{
+    
+    if(req.file){
+        req.body.avatar=req.file.path;
+    }
+    else{
+        delete req.body.avatar;
+    }
+    const existRecord=await AccountAdmin.findOne({
+        _id:req.account.id,
+        deleted:false
+    })
+    if(existRecord){
+        const updateData={
+            fullName:req.body.fullName,
+            email:req.body.email,
+            phone:req.body.phone
+        };
+        if(req.body.avatar){
+            updateData.avatar=req.body.avatar;
+        }
+        await AccountAdmin.updateOne({_id:existRecord._id},updateData);
+    }
+    req.flash("success","Cập nhật thông tin cá nhân thành công");
+    res.json({
+        code:"success",
+    })
+
 }
--- routes/admin/profile.route.js
+++ routes/admin/profile.route.js
@@ -1,5 +1,9 @@
 const router=require('express').Router();
+const multer=require('multer');
+const cloudinaryHelper=require('../../helpers/cloudinary.helper');
+const upload=multer({storage:cloudinaryHelper.storage});
 const profileController=require('../../controllers/admin/profile.controller');
 router.get('/edit',profileController.edit);
+router.patch('/edit',upload.single('avatar'),profileController.editPatch);
 router.get('/change-password',profileController.changePassword);
 module.exports=router;
