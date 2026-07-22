20d61c150f62341b0189cccef3aaa7632cfd17d1
Bùi Bùi <huyphuoc09112005@gmail.com>
2026-07-22 09:24:43 +0700

    xóa đăng ký admin

--- controllers/admin/account.controller.js
+++ controllers/admin/account.controller.js
@@ -10,40 +10,6 @@ module.exports.login= async(req,res)=>{
         pageTitle:"Đăng nhập"
     })
 }
-module.exports.register=async(req,res)=>{
-    res.render("admin/pages/register.pug",{
-        pageTitle:"Đăng ký"
-    })
-}
-module.exports.registerPost=async(req,res)=>{
-    const {fullName,email,password}=req.body;
-    const existAccount=await AccountAdmin.findOne({
-        email:email
-    });
-    if(existAccount){
-        res.json({
-            code:"error",
-            message:"Email đã tồn tại trong hệ thống"
-        });
-        return;
-    }
-    
-    const salt=await bcrypt.genSalt(10);
-
-    const hashPassword=await bcrypt.hash(password,salt);
-
-    const newAccount=new AccountAdmin({
-        fullName:fullName,
-        email:email,
-        password:hashPassword,
-        status:"initial"
-    });
-    await newAccount.save();
-    res.json({
-        code:"success",
-        message : "Đăng ký tài khoản thành công"
-    });
-}
 module.exports.loginPost=async(req,res)=>{
     const {email,password,rememberPassword}=req.body;
     console.log(email);
@@ -189,12 +155,6 @@ module.exports.otpPasswordPost=async(req,res)=>{
         message:"Xác thực OTP thành công"
     })
 }
-module.exports.registerInitial=async(req,res)=>{
-    res.render("admin/pages/register-initial.pug",{
-        pageTitle:"Tài khoản đã được khởi tạo"
-    })
-}
-
 module.exports.resetPassword=async(req,res)=>{
     res.render("admin/pages/reset-password.pug",{
         pageTitle:"Đổi mật khẩu"
